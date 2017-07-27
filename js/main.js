12a331a476a228ec2b7bf4eaede8c88b4385fbb9
sofianepro <thesofianexp@hotmail.com>
2017-07-27 20:55:49 +0100

    Work with about me page ( not finished yet )

--- js/main.js
+++ js/main.js
@@ -5,13 +5,9 @@ $(function () {
 	$(window).resize(function() {$('.intro').height($(window).height());})
 
 	// Adjust intro item Center
-	$('.intro .wrapper').css('marginTop', ($(window).height() - $('.wrapper').height()) / 2.4 );
-
-	$('.intro .welcome').css('paddingTop', ($(window).height() - $('.welcome').height()) / 2 );
+	$('.intro .welcome').css('marginTop', ($(window).height() - $('.welcome').height()) / 2.4 );
 
   	$(window).resize(function(){
-  		$('.intro .wrapper').css('marginTop', ($(window).height() - $('.wrapper').height()) / 2.4 );
-    	$('.intro .welcome').css('paddingTop', ($(window).height() - $('.welcome').height()) / 2 );
   	})
 
 
